f707c917da9ba55bb4cf6e770bdd0fa65704ec79
DaShaun <dashaun@dashaun.com>
2021-12-09 21:07:40 -0600

    reorder vars, refactor provider

--- variables.tf
+++ variables.tf
@@ -1,11 +1,21 @@
-variable "cloudflare_zone_id" {
-  default   = "b64c1a7fc27f0e75b556d19a36eb4ca6"
+variable "cloudflare_api_token" {
   type      = string
   sensitive = true
 }
 
 variable "cloudflare_domain" {
   default   = "javagrunt.com"
+  type      = string
+  sensitive = true
+}
+
+variable "cloudflare_email" {
+  type      = string
+  sensitive = true
+}
+
+variable "cloudflare_zone_id" {
+  default   = "b64c1a7fc27f0e75b556d19a36eb4ca6"
   type      = string
   sensitive = true
 }
